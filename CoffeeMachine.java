cc74f3546c1e4aca95d6a8321a044b7b4e2d7332
Nickolay Kotin <nickolaykotin35@yandex.ru>
2020-04-30 20:11:03 +0300

    Third step

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -12,6 +12,7 @@ public class CoffeeMachine {
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
         calculate();
+        calculateAmount();
     }
 
     public static void calculate(){
@@ -24,5 +25,31 @@ public class CoffeeMachine {
         System.out.println(water + " ml of water");
         System.out.println(milk + " ml of milk");
         System.out.println(coffee + " g of coffee beans");
+    }
+
+    public static void calculateAmount(){
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int waterMl = sc.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milkMl = sc.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
+        int coffeeG = sc.nextInt();
+        System.out.println("Write how many cups of coffee you will need: ");
+        int amountcups = sc.nextInt();
+
+        int water = waterMl / 200;
+        int milk = milkMl / 50;
+        int coffee = coffeeG / 15;
+
+        int limit = Math.min(water, Math.min(milk, coffee));
+
+        if (amountcups > limit) {
+            System.out.println("No, I can make only " + limit + " cup(s) of coffee");
+        } else if (amountcups == limit) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else {
+            int extra = limit - amountcups;
+            System.out.println("Yes, I can make that amount of coffee (and even " + extra + " more than that)");
+        }
     }
 }
